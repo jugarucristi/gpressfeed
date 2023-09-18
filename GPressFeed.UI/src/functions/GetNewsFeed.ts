e3f396d3e3d63643c2614d16e17c91c728126cb4
Cristi Jugaru <31081854+jugarucristi@users.noreply.github.com>
2023-09-18 22:46:05 +0300

    Fixed API url, added Cloudflare tunnel

--- GPressFeed.UI/src/functions/GetNewsFeed.ts
+++ GPressFeed.UI/src/functions/GetNewsFeed.ts
@@ -9,7 +9,7 @@ export default async function GetNewsFeed(): Promise<NewsFeed>
                 Accept: 'application/json',
             }
         };
-        const response = await fetch('http://localhost/api/pressfeed/', requestSettings)
+        const response = await fetch('https://www.gpressfeed.com/api/pressfeed/', requestSettings)
 
         if(!response.ok)
             throw new Error(`Error! status: ${response.status}`);
