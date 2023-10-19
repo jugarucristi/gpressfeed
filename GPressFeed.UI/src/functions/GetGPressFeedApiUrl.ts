4a446cc780c73d1886ef44bc91fd9c32304915e0
Cristi Jugaru <31081854+jugarucristi@users.noreply.github.com>
2023-10-19 16:42:56 +0300

    Fixed GetApiUrl bug

--- GPressFeed.UI/src/functions/GetGPressFeedApiUrl.ts
+++ GPressFeed.UI/src/functions/GetGPressFeedApiUrl.ts
@@ -4,7 +4,7 @@ import { PUBLIC_GPRESSFEED_LOCAL_API_URL } from "$env/static/public";
 
 export default function GetGPressFeedApiUrl(): string {
   if (PUBLIC_IS_DEVELOPMENT_ENV == "True") {
-    return PUBLIC_GPRESSFEED_API_URL;
+    return PUBLIC_GPRESSFEED_LOCAL_API_URL;
   }
-  return PUBLIC_GPRESSFEED_LOCAL_API_URL;
+  return PUBLIC_GPRESSFEED_API_URL;
 }
